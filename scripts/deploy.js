7155a64ef07d489c230cd51bd1ba69947ae0c6ec
omkarg01 <omkargujja01@gmail.com>
2022-10-25 23:35:22 +0530

    done deployment

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -2,6 +2,9 @@
 const { ethers } = require("hardhat");
 
 async function main() {
+  const [deployer] = await ethers.getSigners();
+  console.log("Deploying contracts with the account:", deployer.address);
+  console.log("Account balance:", (await deployer.getBalance()).toString());
   /*
 A ContractFactory in ethers.js is an abstraction used to deploy new smart contracts,
 so nftContract here is a factory for instances of our GameItem contract.
@@ -15,7 +18,7 @@ so nftContract here is a factory for instances of our GameItem contract.
   await deployedNftContract.deployed();
 
   // print the address of the deployed contract
-  console.log("NFT Contract Address: ", firstContract.address);
+  console.log("NFT Contract Address: ", deployedNftContract.address);
 }
 
 // call the main function
